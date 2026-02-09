5801bb8d5ac0236464836bc40de58062147fd1e9
Abirokaya <rokayaabinash9@gmail.com>
2026-02-10 00:39:01 +0545

    dafd

--- config/settings.py
+++ config/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =os.environ.get("SECRET_KEY")
 import os
+SECRET_KEY =os.environ.get("SECRET_KEY")
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
